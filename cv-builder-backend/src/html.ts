9971fc1691b9eaa2faeef77bc9017b66acfe658a
Eugene <jaskon97@gmail.com>
2023-11-27 02:08:35 +0100

    PDF is built correctly!

--- cv-builder-backend/src/html.ts
+++ cv-builder-backend/src/html.ts
@@ -4,12 +4,23 @@ export default function html() {
         <head>
             <title>CV Builder</title>
             <style>
+                * {
+                    box-sizing: border-box;
+                    margin: 0;
+                    padding: 0;
+                }
                 body {
                     font-family: sans-serif;
+                    font-size: 12pt;
                     text-align: center;
+                    -webkit-print-color-adjust: exact;
                 }
                 h1 {
                     color: darkgreen;
+                    font-size: 20pt;
+                }
+                p {
+                    font-size: 20pt;
                 }
                 a {
                     color: red;
@@ -17,15 +28,76 @@ export default function html() {
                 .separator {
                     background-color: grey;
                     height: 2px;
-                    width: 90%;
+                    margin: 30px;
+                    width: 100%;
+                }
+                
+                .global-wrapper {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    padding: 10px;
+                }
+                .header {
+                    background-color: black;
+                    border-radius: 10px;
+                    height: 100px;
+                    width: 100%;
+                }
+                .content-wrapper {
+                    align-items: stretch;
+                    display: flex;
+                    flex-direction: row;
+                    padding: 10px;
+                    width: 100%;
+                }
+                .left-wrapper {
+                    align-items: center;
+                    display: flex;
+                    flex-direction: column;
+                    font-size: 12pt;
+                    padding: 0 10px 0 0;
+                    width: 60%;
+                }
+                .right-wrapper {
+                    align-items: center;
+                    display: flex;
+                    flex-direction: column;
+                    padding: 0 0 0 10px;
+                    width: 40%;
+                }
+                .separator-vertical {
+                    background-color: grey;
+                    width: 2px;
                 }
             </style>
         </head>
         <body>
-            <h1>CV Builder</h1>
-            <p>Click <a href="/api/cv">here</a> to get your CV.</p>
-            <div class="separator"></div>
-            <p>Click <a href="/api/cv?format=pdf">here</a> to get your CV as a PDF.</p>
+            <div class="global-wrapper">
+                <header class="header"></header>
+                <div class="content-wrapper">
+                    <div class="left-wrapper">
+                        <h1>CV Builder</h1>
+                        <p>Click <a href="https://google.com">here</a> to get your CV.</p>
+                        <div class="separator"></div>
+                        <p>Click <a href="https://google.com">here</a> to get your CV as a PDF.</p>
+                    </div>
+                    <div class="separator-vertical"></div>
+                    <div class="right-wrapper">
+                        <div>Right panel</div>
+                        <div>Right panel content</div>
+                        <div><input type="checkbox"> Checkbox</div>
+                        <div><input type="checkbox" checked> Checkbox checked</div>
+                        <div>
+                            <ul>
+                                <li>Item 1</li>
+                                <li>Item 2</li>
+                                <li>Item 3</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </body>
         </html>
     `;
--- cv-builder-backend/src/index.ts
+++ cv-builder-backend/src/index.ts
@@ -2,16 +2,17 @@ import puppeteer from 'puppeteer';
 import html from './html';
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
     await page.setContent(html());
-    await page.setViewport({ width: 1920, height: 1080 });
+    // await page.goto('https://google.com');
+    await page.setViewport({ width: 795, height: 1124 });
     console.log('page loaded');
 
     await page.pdf({ path: 'exported.pdf', format: 'A4' });
     console.log('pdf generated');
 
-    await browser.close();
-    console.log('browser closed');
+    // await browser.close();
+    // console.log('browser closed');
 })();
