52a5055a41e04ab4eb7bca755d6ae0b66864eb8b
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-12-18 23:04:02 +0100

    refactor: set content width, resize controls and preview zones

--- cv-builder-ui/src/app/page.tsx
+++ cv-builder-ui/src/app/page.tsx
@@ -18,11 +18,11 @@ export default function Home() {
     const [template, setTemplate] = useState<Template>(Template.default);
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between py-4">
-            <div className="flex flex-row items-stretch gap-8 content w-fit">
-                <div className="grow-1 w-[400px]">
+        <main className="flex min-h-screen flex-col items-center justify-center py-4 w-[fit]">
+            <div className="flex flex-row items-stretch gap-8 content w-[1500px]">
+                <div className="grow-1 w-[900px]">
                     <ControlsComponent
-                        className="fixed w-[400px] overflow-y-auto top-0 h-full pt-8 pb-2 box-border px-4"
+                        className="sticky overflow-y-auto top-0 h-full pt-8 pb-2 box-border px-4"
                         content={content}
                         setContent={setContent}
                         template={template}
@@ -33,12 +33,12 @@ export default function Home() {
 
                 <div className="bg-gray-300 w-0.5 my-4"></div>
 
-                <div className="border-black border box-content p-5 w-[795px] h-[1124px]">
+                <div className="border-black border box-content p-5 w-[500px] h-[1124px]">
 
                     <iframe
                         className="w-full h-full border border-black"
                         srcDoc={generateCvHtml(content, template)}
-                    ></iframe>
+                    />
                 </div>
             </div>
         </main>
